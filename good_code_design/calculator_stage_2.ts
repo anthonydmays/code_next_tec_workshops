03325bf269c1f6d65ab06378175612bf4d4a1be3
Anthony D. Mays <anthonydmays@users.noreply.github.com>
2022-12-08 01:30:57 -0800

    Create calculator_stage_2.ts

---
+++ good_code_design/calculator_stage_2.ts
@@ -0,0 +1,55 @@
+class Calculator {
+  private isOn = false;
+  private isScientific = false;
+  private result = 0;
+
+  doOperation(operation: string, x: number, y: number) {
+    if (!this.isOn) {
+      return false;
+    }
+
+    if (this.isScientific) {
+      switch (operation) {
+        case '^':
+          this.result = x ^ y;
+          this.updateDisplay(`${x} ^ ${y} = ${this.result}`);
+          break;
+        default:
+          throw new Error('Operation not recognized.');
+      }
+    }
+
+    switch (operation) {
+      case '+':
+        this.result = x + y;
+        this.updateDisplay(`${x} + ${y} = ${this.result}`);
+        break;
+      case '-':
+        this.result = x - y;
+        this.updateDisplay(`${x} - ${y} = ${this.result}`);
+        break;
+      case '*':
+        this.result = x * y;
+        this.updateDisplay(`${x} * ${y} = ${this.result}`);
+        break;
+      case '/':
+        this.result = x / y;
+        this.updateDisplay(`${x} / ${y} = ${this.result}`);
+        break;
+      default:
+        throw new Error('Operation not recognized.');
+    }
+  }
+  
+  updateDisplay(text: string) {
+    console.log(text);
+  }
+  
+  powerOn() {
+    this.isOn = true;
+  }
+  
+  powerOff() {
+    this.isOn = false;
+  }
+}
